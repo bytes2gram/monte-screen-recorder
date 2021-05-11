ba0eaf6d65c611c997d86d66f7ed09dc97853e5d
Dinesh Kuppan <email2dineshkuppan@gmail.com>
2021-05-11 12:09:07 +0530

    Refactored & Organize Import statements. And updated the dependencies inside pom.xml

---
+++ src/main/java/org/bytes2gram/monte/ScreenRecorder.java
@@ -0,0 +1,31 @@
+package org.bytes2gram.monte;
+
+import org.bytes2gram.monte.screenrecorder.ScreenRecorderCompactMain;
+
+/*
+ * @(#)org.bytes2gram.monte.ScreenRecorder.java  1.0  2011-03-19
+ * 
+ * Copyright (c) 2011 Werner Randelshofer, Immensee, Switzerland.
+ * All rights reserved.
+ * 
+ * You may not use, copy or modify this file, except in compliance with the
+ * license agreement you entered into with Werner Randelshofer.
+ * For details see accompanying license terms.
+ */
+
+/**
+ * {@code org.bytes2gram.monte.ScreenRecorder}.
+ *
+ * @author Werner Randelshofer
+ * @version 1.0 2011-03-19 Created.
+ */
+public class ScreenRecorder {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       ScreenRecorderCompactMain.main(args);
+    }
+
+}
